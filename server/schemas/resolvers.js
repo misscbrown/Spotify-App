72d633d7b33897d1acf7b25005dd611e9a7acf60
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-06-10 15:48:21 +0100

    updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { User, Post } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
-const checkAuth = require("../../server/utils/auth")
+const checkAuth  = require("../../server/utils/auth")
 
 const resolvers = {
 
