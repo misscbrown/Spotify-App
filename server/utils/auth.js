8fe7282819d8a86b2846793fb86309570862958c
cee breezy <djmisscbrown@hotmail.com>
2022-06-10 14:33:48 +0100

    added module exports context

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,32 @@
+const { AuthenticationError } = require('apollo-server');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
+//secret key 
+// const { SECRET_KEY } = require('../config')
+
+module.exports = (context) => {
+  const authHeader = context.req.headers.authorization;
+  if (authHeader) {
+    // Bearer
+    const token = authHeader.split('Bearer ')[1];
+    if (token) {
+      try {
+        const user = jwt.verify(token, SECRET_KEY);
+        return user;
+      } catch (err) {
+        throw new AuthenticationError('Invalid/Expired token');
+        
+      }
+    }
+    throw new Error('Authentication token must be \'Bearer [token]');
+    
+  }
+  throw new Error('Authentication header must be provided');
+}
+
 
 module.exports = {
   // function for our authenticated routes
