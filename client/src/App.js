aa0d0525b06f18b5b9f4e00de5ffc96b8bc7c7e7
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-06-11 10:14:43 +0100

    updated update App/js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
+import Dashboard from './pages/Dashboard';
+import AllPost from './pages/AllPost';
+import Navbar from './components/Navbar';
+
 import './App.css';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+  
+});
+
+const authLink = setContext((_, { headers }) => {
+
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+      
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <Router>
+        <Navbar />
+        <Routes>
+          <Route 
+            path='/' 
+            element={<Dashboard />} 
+          />
+          <Route 
+            path='/allPosts' 
+            element={<AllPost />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+           <Route 
+            path='/SignupForm'
+            element={<SignupForm />}
+          />
+           <Route 
+            path='/LoginForm'
+            element={<LoginForm />}
+          />
+        </Routes>
+    </Router>
+    </ApolloProvider>
   );
 }
 
