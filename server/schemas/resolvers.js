6e542aa6765fef03f8c4647901c35415599371f2
cee breezy <djmisscbrown@hotmail.com>
2022-06-11 11:44:14 +0100

    added login to mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,56 +1,53 @@
-const { User, Post } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
-
-
+const { User, Post } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-
-    Query: {
-
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password')
-                return userData;
-            }
-            throw new AuthenticationError('Not logged in');
-        }
-
-        // // @Check code
-        // getPosts: async () => {
-        //     try {
-        //         const posts = await Post.findAll();
-        //         return posts;
-        //     }
-        //     catch (err) {
-        //         throw new Error(err);;
-        //     }
-        // },
-        // async getPost(_, { postId }) {
-        //     try {
-        //         const post = await Post.findById(postId);
-        //         if (post) {
-        //             return post;
-        //         } else {
-        //             throw new Error('Post not found');
-        //         }
-        //     } catch (err) {
-        //         throw new Error(err);
-        //     }
-        // },
-
-        
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
     },
 
-    Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
+    // // @Check code
+    // getPosts: async () => {
+    //     try {
+    //         const posts = await Post.findAll();
+    //         return posts;
+    //     }
+    //     catch (err) {
+    //         throw new Error(err);;
+    //     }
+    // },
+    // async getPost(_, { postId }) {
+    //     try {
+    //         const post = await Post.findById(postId);
+    //         if (post) {
+    //             return post;
+    //         } else {
+    //             throw new Error('Post not found');
+    //         }
+    //     } catch (err) {
+    //         throw new Error(err);
+    //     }
+    // },
+  },
 
-            return { token, user };
-        },
-    
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (_, { email, password }) => {
+      return null;
+    },
 
     //     createPost: async (_, { body }, context) => {
     //         const user = checkAuth(context);
@@ -62,11 +59,10 @@ const resolvers = {
     //             username: user.username,
     //             createdAt: new Date().toISOString()
     //         });
-            
+
     //         const post = await newPost.save();
 
     //         return post;
-
 
     // },
 
@@ -79,13 +75,11 @@ const resolvers = {
     //           if(user.username === post.username){
     //               await post.findByIdandDelete(postId);
     //               return 'Post deleted sucessfully';
-    //           } 
+    //           }
     //               throw new AuthenticationError("You are not autherised to delete post")
-              
+
     //         }
-        
-        
-    },
+  },
 };
 
 module.exports = resolvers;
