1d8a3dcba6e344b372871d70f8e9f2e58b92b1ca
Fary <farhiya.i.mahamud@gmail.com>
2022-06-11 13:50:58 +0100

    edited form validity

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,105 +1,96 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignupForm = () => {
-    // set initial form state
-  const [userFormData, setUserFormData] = useState({ 
-    username: '', 
-    email: '', 
-    password: '' 
+  const navigate = useNavigate();
+  // set initial form state
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
   });
   const [addUser, { error }] = useMutation(ADD_USER);
-    // set state for form validation
-    const [validated] = useState(false);
-    // set state for alert
-    const [showAlert, setShowAlert] = useState(false);
-  
-    const handleInputChange = (event) => {
-      const { name, value } = event.target;
-  
-      setUserFormData({ 
-        ...userFormData, 
-        [name]: value,
-       });
-    };
-  
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-       // check if form has everything (as per react-bootstrap docs)
-      const form = event.currentTarget;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-  
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setUserFormData({
+      ...userFormData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity()) {
       try {
         const { data } = await addUser({
           variables: { ...userFormData },
         });
-  
+
         Auth.login(data.addUser.token);
+
+        navigate("/dashboard");
       } catch (e) {
         console.error(e);
-  
       }
-  
-      setUserFormData({
-        username: '',
-        email: '',
-        password: '',
-      });
-    };
 
-    return (
-        <main className="flex-row justify-center mb-4">
+      setUserFormData({
+        username: "",
+        email: "",
+        password: "",
+      });
+    }
+  };
+
+  return (
+    <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
         <div className="card">
           <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
           <div className="card-body">
-            {!addUser ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/Dashboard">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your username"
-                  name="username"
-                  type="text"
-                  value={setUserFormData.name}
-                  onChange={handleInputChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={setUserFormData.email}
-                  onChange={handleInputChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={setUserFormData.password}
-                  onChange={handleInputChange}
-                />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className="form-input"
+                placeholder="Your username"
+                name="username"
+                type="text"
+                value={setUserFormData.username}
+                onChange={handleInputChange}
+              />
+              <input
+                className="form-input"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                value={setUserFormData.email}
+                onChange={handleInputChange}
+              />
+              <input
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                value={setUserFormData.password}
+                onChange={handleInputChange}
+              />
+              <button
+                className="btn btn-block btn-primary"
+                style={{ cursor: "pointer" }}
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
 
             {error && (
               <div className="my-3 p-3 bg-danger text-white">
@@ -110,10 +101,7 @@ const SignupForm = () => {
         </div>
       </div>
     </main>
-
-
-    );
-
+  );
 };
 
 export default SignupForm;
