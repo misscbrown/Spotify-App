c4624ad882823592e4251569c294814b46f15869
Fary <farhiya.i.mahamud@gmail.com>
2022-06-12 16:40:10 +0100

    exported queries for front end use

---
+++ client/src/utils/queries.js
@@ -0,0 +1,51 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      post {
+        _id
+        postText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_POSTS = gql`
+  query getAllPosts {
+    posts {
+      _id
+      postText
+      username
+      createdAt
+      Comment {
+        _id
+        commentText
+        username
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bio
+      image
+      post {
+        _id
+        postText
+        username
+        createdAt
+      }
+    }
+  }
+`;
