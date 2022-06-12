c37a5fe6fae0790888162f901bac117e6b100dac
Fary <farhiya.i.mahamud@gmail.com>
2022-06-12 16:40:32 +0100

    imported mutations for front end use

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,9 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
 
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -16,7 +16,7 @@ mutation login($email: String!, $password: String!) {
 
 // ADD_USER will execute the addUser mutation.
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
@@ -25,5 +25,35 @@ mutation addUser($username: String!, $email: String!, $password: String!) {
       }
     }
   }
-  
+`;
+
+export const ADD_POST = gql`
+  mutation addPosts($postText: String!) {
+    addPosts(postText: $postText) {
+      _id
+      postText
+      username
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($postId: ID!, $commentText: String!) {
+    addComment(postId: $posttId, commentText: $commentText) {
+      _id
+      postText
+      username
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
 `;
