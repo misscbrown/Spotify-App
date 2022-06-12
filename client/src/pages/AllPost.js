d24b460991d5f3c7fcf65edfd3eb854f043a2ecf
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-06-12 21:50:56 +0100

    Added foldes and files for comment list, comment form, post list and post form.  Added code on All post

--- client/src/pages/AllPost.js
+++ client/src/pages/AllPost.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/client';
 
-function AllPost() {
+import PostList from '../components/PostList';
+import PostForm from '../components/PostForm';
+
+import { QUERY_POSTS } from '../utils/queries';
+
+const AllPost = () => {
+  const { loading, data } = useQuery(QUERY_POSTS);
+  const posts = data?.posts || [];
+
   return (
-    <div>
-        ALL POSTS
-    </div>
-  )
-}
+    <main>
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: '1px dotted #1a1a1a' }}
+        >
+          <PostForm />
+        </div>
+        <div className="col-12 col-md-8 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <PostList
+              posts={posts}
+              title="..."
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default AllPost
