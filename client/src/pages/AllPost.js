5a8e5fd4c7e52e4006afb6cb8831b886e29510ec
Fary <farhiya.i.mahamud@gmail.com>
2022-06-13 19:06:51 +0100

    commented out code we will delete

--- client/src/pages/AllPost.js
+++ client/src/pages/AllPost.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+// import React from "react";
+// import { useQuery } from "@apollo/client";
 
-import PostList from '../components/PostList';
-import PostForm from '../components/PostForm';
+// import PostList from "../components/PostList";
+// import PostForm from "../components/PostForm";
 
-import { QUERY_POSTS } from '../utils/queries';
+// import { QUERY_POSTS } from "../utils/queries";
 
-const AllPost = () => {
-  const { loading, data } = useQuery(QUERY_POSTS);
-  const posts = data?.posts || [];
+// const AllPost = () => {
+//   console.log("WHAT THE HELL");
+//   const { loading, data } = useQuery(QUERY_POSTS);
+//   const posts = data?.posts || [];
 
-  return (
-    <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <PostForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <PostList
-              posts={posts}
-              title="..."
-            />
-          )}
-        </div>
-      </div>
-    </main>
-  );
-};
+//   return (
+//     <main>
+//       <div className="flex-row justify-center">
+//         <div
+//           className="col-12 col-md-10 mb-3 p-3"
+//           style={{ border: "1px dotted #1a1a1a" }}
+//         >
+//           <PostForm />
+//         </div>
+//         <div className="col-12 col-md-8 mb-3">
+//           {loading ? (
+//             <div>Loading...</div>
+//           ) : (
+//             <PostList posts={posts} title="..." />
+//           )}
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
 
-export default AllPost
+// export default AllPost;
