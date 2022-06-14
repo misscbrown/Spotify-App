124e26164ed63ed7b1d727df8104432e7b10d391
Fary <farhiya.i.mahamud@gmail.com>
2022-06-14 12:16:08 +0100

    switched nav bar compnent back to how it was

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,64 +4,64 @@ import { Link } from "react-router-dom";
 import SignupForm from "./SignupForm";
 import LoginForm from "./LoginForm";
 
-// const AppNavbar = () => {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <Link className="navbar-brand" to="/"></Link>
-//       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//         <ul className="navbar-nav mr-auto">
-//           <li className="nav-item">
-//             <Link className="nav-link" to="/">
-//               <h1>Homepage</h1>
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link" to="/dashboard">
-//               <h1>Dashboard</h1>
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link" to="/allPosts">
-//               <h1>All Posts</h1>
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link" to="/signup">
-//               <h1>Sign up</h1>
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link" to="/login">
-//               <h1>Login </h1>
-//             </Link>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// };
-
 const AppNavbar = () => {
   return (
-    <nav>
-      <div class="container">
-        <div class="theme-switch">
-          <div class="switch"></div>
-        </div>
-        <div class="navigation">
-          <ul>
-            <link to="/" class="active" target="_blank">
-              Home
-            </link>
-            <link to="/dashboard">Dashboard</link>
-            <link to="/allpost" target="_blank">
-              All Posts
-            </link>
-          </ul>
-        </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/"></Link>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              <h1>Homepage</h1>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              <h1>Dashboard</h1>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/allPosts">
+              <h1>All Posts</h1>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/signup">
+              <h1>Sign up</h1>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              <h1>Login </h1>
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
 };
+
+// const AppNavbar = () => {
+//   return (
+//     <div class="container">
+//       <div class="theme-switch">
+//         <div class="switch"></div>
+//       </div>
+//       <div class="navigation">
+//         <ul>
+//           <a href="/" class="active" target="_blank">
+//             Home
+//           </a>
+//           <a href="/dashboard" target="_blank">
+//             Dashboard
+//           </a>
+//           <a href="/allpost" target="_blank">
+//             All Posts
+//           </a>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default AppNavbar;
