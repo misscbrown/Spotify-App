928a3a1437f4329570a9d97c356c72d8b97042ab
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-06-14 13:58:45 +0100

    Added the add post and comments

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,54 +1,54 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// const PostList = ({
-//   posts,
-//   title,
-//   showTitle = true,
-//   showUsername = true,
-// }) => {
-//   if (!posts.length) {
-//     return <h3>No Post Yet</h3>;
-//   }
+const PostList = ({
+  posts,
+  title,
+  showTitle = true,
+  showUsername = true,
+}) => {
+  if (!posts.length) {
+    return <h3>No posts Yet</h3>;
+  }
 
-//   return (
-//     <div>
-//       {showTitle && <h3>{title}</h3>}
-//       {posts &&
-//         posts.map((post) => (
-//           <div key={post._id} className="card mb-3">
-//             <h4 className="card-header bg-primary text-light p-2 m-0">
-//               {showUsername ? (
-//                 <Link
-//                   className="text-light"
-//                   to={`/profiles/${post.username}`}
-//                 >
-//                   {post.username} <br />
-//                   <span style={{ fontSize: '1rem' }}>
-//                     post created at {post.createdAt}
-//                   </span>
-//                 </Link>
-//               ) : (
-//                 <>
-//                   <span style={{ fontSize: '1rem' }}>
-//                     {post.createdAt}
-//                   </span>
-//                 </>
-//               )}
-//             </h4>
-//             <div className="card-body bg-light p-2">
-//               <p>{post.postText}</p>
-//             </div>
-//             <Link
-//               className="btn btn-primary btn-block btn-squared"
-//               to={`/thoughts/${post._id}`}
-//             >
+  return (
+    <div>
+      {showTitle && <h3>{title}</h3>}
+      {posts &&
+        posts.map((post) => (
+          <div key={post._id} className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              {showUsername ? (
+                <Link
+                  className="text-light"
+                  to={`/dashboard/${post.username}`}
+                >
+                  {post.postAuthor} <br />
+                  <span style={{ fontSize: '1rem' }}>
+                    had this post on {post.createdAt}
+                  </span>
+                </Link>
+              ) : (
+                <>
+                  <span style={{ fontSize: '1rem' }}>
+                    You had this post on {post.createdAt}
+                  </span>
+                </>
+              )}
+            </h4>
+            <div className="card-body bg-light p-2">
+              <p>{post.postText}</p>
+            </div>
+            <Link
+              className="btn btn-primary btn-block btn-squared"
+              to={`/posts/${post._id}`}
+            >
+              Join the discussion
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
 
-//             </Link>
-//           </div>
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default PostList;
+export default PostList;
--- client/src/pages/AllPost.js
+++ client/src/pages/AllPost.js
@@ -1,37 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 
-// import PostList from "../components/PostList";
-// import PostForm from "../components/PostForm";
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 
-// import { QUERY_POSTS } from "../utils/queries";
+import { ADD_POST } from '../utils/mutations';
+import { QUERY_POSTS, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth.js';
 
 const AllPost = () => {
-  console.log("WHAT THE HELL");
 
-  return <div> ALL POST</div>;
-  //   const { loading, data } = useQuery(QUERY_POSTS);
-  //   const posts = data?.posts || [];
+  const [postText, setpostText] = useState('');
 
-  //   return (
-  //     <main>
-  //       <div className="flex-row justify-center">
-  //         <div
-  //           className="col-12 col-md-10 mb-3 p-3"
-  //           style={{ border: "1px dotted #1a1a1a" }}
-  //         >
-  //           <PostForm />
-  //         </div>
-  //         <div className="col-12 col-md-8 mb-3">
-  //           {loading ? (
-  //             <div>Loading...</div>
-  //           ) : (
-  //             <PostList posts={posts} title="..." />
-  //           )}
-  //         </div>
-  //       </div>
-  //     </main>
-  //   );
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addPost, { error }] = useMutation(ADD_POST, {
+    update(cache, { data: { addPost } }) {
+      try {
+        const { posts } = cache.readQuery({ query: QUERY_POSTS });
+
+        cache.writeQuery({
+          query: QUERY_POSTS,
+          data: { posts: [addPost, ...posts] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      // update me object's cache
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, posts: [...me.posts, addPost] } },
+      });
+    },
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addPost({
+        variables: {
+          postText,
+          username: Auth.getProfile().data.username,
+        },
+      });
+
+      setpostText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'postText' && value.length <= 280) {
+      setpostText(value);
+      setCharacterCount(value.length);
+    }
+  };
+
+  return (
+    <div>
+     
+
+      {Auth.loggedIn() ? (
+        <>
+          <p
+            className={`m-0 ${
+              characterCount === 280 || error ? 'text-danger' : ''
+            }`}
+          >
+            Character Count: {characterCount}/280
+          </p>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="postText"
+                placeholder="Here's a new post..."
+                value={postText}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" type="submit">
+                Add POST
+              </button>
+            </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
+              </div>
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          Please login{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default AllPost;
