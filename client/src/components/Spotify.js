2038b197462dc5bba10c1226dae803fbd4ed9495
cee breezy <djmisscbrown@hotmail.com>
2022-06-16 20:51:38 +0100

    imported spotify and built function

---
+++ client/src/components/Spotify.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Spotify = ({
+    link,
+    style = {},
+    wide = false,
+    width = wide ? '100%' : 300,
+    height = wide ? 80 : 380,
+    frameBorder = 0,
+    allow = 'encrypted-media',
+    ...props
+}) => {
+  const url = new URL(link);
+  // https://open.spotify.com/track/1KFxcj3MZrpBGiGA8ZWriv?si=f024c3aa52294aa1
+  return (
+    <iframe
+      title="Spotify Web Player"
+      src={`https://open.spotify.com/embed${url.pathname}`}
+      width={width}
+      height={height}
+      frameBorder={frameBorder}
+      allow={allow}
+      style={{
+        borderRadius: 8,
+        ...style,
+      }}
+      {...props}
+    />
+  );
+};
+
+export default Spotify;
