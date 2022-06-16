65d4af7a84c217a46bc463cee9ae00b6e94d07f7
cee breezy <djmisscbrown@hotmail.com>
2022-06-16 20:52:54 +0100

    tested out spotify link and returned div

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 
 import Auth from "../utils/auth.js";
 
+// import Spotify from "react-spotify-embed"
+
 const Dashboard = () => {
-  return <div>DASHBOARD</div>;
+  return (
+    <div>
+      {/* <Spotify link="https://open.spotify.com/track/44JSreJxvgl3CxLo5MoQL0"/> */}
+    </div>
+  );
 };
 export default Dashboard;
