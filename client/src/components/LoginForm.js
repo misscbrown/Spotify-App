16afc5e87f745009805ba0e78c4499285cc65c40
cee breezy <djmisscbrown@hotmail.com>
2022-06-16 13:49:05 +0100

    added styling for page

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -51,36 +51,50 @@ const LoginForm = () => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-                             
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+    <main className="flex-row justify-center mb-4">                      
+      <div className="relative py-16
+        before:absolute before:inset-0 before:w-full before:h-[50%] before:bg-black">
+        <div className="relative container m-auto px-6 text-gray-500 md:px-12 xl:px-40">
+          <div class="rounded-xl border bg-opacity-50 backdrop-blur-2xl bg-white shadow-xl">
+          <div class="p-6 sm:p-16"><h2 class="mb-8 text-2xl text-cyan-900 font-bold">Login in to your account</h2>
+        <div className="">
           <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
+                  <form className="space-y-8" onSubmit={handleFormSubmit}>
+                   
+                    <input     
+                className="block w-full px-4 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
+                focus:ring-2 focus:ring-sky-300 focus:outline-none
+                invalid:ring-2 invalid:ring-red-400"
                 placeholder="Your email"
                 name="email"
                 type="email"
+                      
                 value={setUserFormData.email}
                 onChange={handleInputChange}
               />
               <input
                 className="form-input"
-                placeholder="******"
+                placeholder="password"
                 name="password"
                 type="password"
                 value={setUserFormData.password}
                 onChange={handleInputChange}
+                class="block w-full px-4 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
+                focus:ring-2 focus:ring-sky-300 focus:outline-none
+                invalid:ring-2 invalid:ring-red-400"
               />
               <button
                 className="btn btn-block btn-primary"
                 style={{ cursor: "pointer" }}
                 type="submit"
-              >
-                Submit
-              </button>
+                class="w-full py-3 px-6 rounded-md bg-green-500
+               focus:bg-sky-700 active:bg-sky-500">
+                <span class="text-white">Continue</span>
+                </button>
+                <p class="border-t pt-6 text-sm">
+                Don't have an account?  
+                <a href="#" class="text-sky-500"> Sign up</a>
+                </p>     
             </form>
 
             {error && (
@@ -90,7 +104,11 @@ const LoginForm = () => {
             )}
           </div>
         </div>
+        </div>
+        </div>
+        </div>
       </div>
+      
     </main>
   );
 };
