72895acc6a038ae9c64b3abb89aa34c180dec6f4
Fary <farhiya.i.mahamud@gmail.com>
2022-06-17 23:35:20 +0100

    changed styling

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,26 +4,20 @@ import { Link } from "react-router-dom";
 
 import auth from "../utils/auth";
 import homeimage from "../../src/assets/images/homepageimage.png";
+import Navbar from "../components/Navbar";
 
 function Homepage() {
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (auth.loggedIn()) {
-  //     navigate("/dashboard");
-  //   }
-  // }, []);
-
   return (
     <div>
-      <div class="container px-8 md:px-12 xl:max-w-5xl mx-auto lg:px-7 xl:px-0">
-        <div class="md:flex md:gap-12 pt-40 lg:py-56">
-          <div class="md:w-6/12">
-            <h1 class="text-gray-900 font-bold text-4xl xl:text-5xl">
-              Build in your way but with our experts{" "}
-              <span class="text-green-500">Support.</span>
+      <Navbar />
+      <div className="container px-8 md:px-12 xl:max-w-5xl mx-auto lg:px-7 xl:px-0">
+        <div className="md:flex md:gap-12 pt-40 lg:py-56">
+          <div className="md:w-6/12">
+            <h1 className="text-gray-900 font-bold text-4xl xl:text-5xl">
+              A place to connect with other{" "}
+              <span className="text-green-500">Music Lovers.</span>
             </h1>
-            <p class="mt-8 text-gray-700">
+            <p className="mt-8 text-gray-700">
               Share and discover songs from your favourite established and
               independent artists and join the conversation
             </p>
@@ -34,8 +28,8 @@ function Homepage() {
               Signup
             </Link>
           </div>
-          <div class="mt-12 h-56 mr-0 md:-mr-24 overflow-hidden sm:h-72 md:h-auto md:mt-0 lg:mr-0 lg:-mt-12 md:w-7/12">
-            <div class="md:5/12 lg:w-6/12 mx-w-l">
+          <div className="mt-12 h-56 mr-0 md:-mr-24 overflow-hidden sm:h-72 md:h-auto md:mt-0 lg:mr-0 lg:-mt-12 md:w-7/12">
+            <div className="md:5/12 lg:w-6/12">
               <img
                 className="homepgpic"
                 src={homeimage}
