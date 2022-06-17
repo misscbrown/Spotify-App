6e86d2a5121322b18b8256a0ece669dcc829af66
Fary <farhiya.i.mahamud@gmail.com>
2022-06-17 23:35:39 +0100

    imported navbar app

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import { Center } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import image from "../assets/dashboard.png";
+import Navbar from "../components/Navbar";
 import Auth from "../utils/auth.js";
 
 const Dashboard = () => {
@@ -11,12 +12,15 @@ const Dashboard = () => {
   };
   return (
     <div>
-      <h1 className="text-center text-5xl">
-        {Auth.getProfile().data.username}
-      </h1>
-      <h2 className="text-center text-3xl">Bio</h2>
-      <div className="w-7/12">
-        <img className="" src={image} />
+      <Navbar />
+      <div className="container px-8 md:px-12 xl:max-w-5xl mx-auto lg:px-7 xl:px-0">
+        <h1 className="text-center text-5xl">
+          {Auth.getProfile().data.username}
+        </h1>
+        <h2 className="text-center text-3xl">Bio</h2>
+        <div className="w-7/12">
+          <img className="" src={image} />
+        </div>
       </div>
     </div>
   );
